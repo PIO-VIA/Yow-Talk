a7af15e0b57fbcde084b37edb2ce6a02b30d58d2
Delmat237 <azangueleonel9@gmail.com>
2025-03-22 12:37:58 +0100

    Ajout de ChatScreen ,AddContactScreen, modification de ChatListCsreen et Login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
 import { Stack } from "expo-router";
 import { MenuProvider } from "react-native-popup-menu";
 
+
+
 export default function RootLayout() {
   return (
     <MenuProvider>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="home" options={{ headerShown: false }} />
+        <Stack.Screen name="chat" options={{ headerShown: false }} />
+        <Stack.Screen name="AddContact" options={{ headerShown: false }} />
       </Stack>
     </MenuProvider>
   );
